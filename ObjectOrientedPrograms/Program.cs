c1b92870a5988c5325b22b44b7ea911668e9fa2b
rahul01r <rrdubeynibm@gmail.com>
2022-11-06 21:43:11 +0530

    [rahul] Add:UC1-Inventory

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿using ObjectOrientedPrograms.Inventorymanagement;
+
+namespace ObjectOrientedPrograms
+{
+    public class Program
+    {
+        const string INVENTORY_DATA_FILE_PATH = @"C:\Users\Lenovo\Desktop\Git\JsonFile\ObjectOrientedPrograms\Inventorymanagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select Programs\n 1.Inventory Management ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Inventory inventory = new Inventory();
+                        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                }
+            }
+        }
+    }
+}
